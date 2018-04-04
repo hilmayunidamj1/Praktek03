748e8205379a554946fb3e4cbaf081d01120e3ad
hilmayunidamj1 <hilmayunidamj@gmail.com>
2018-04-04 07:02:15 -0700

    set atribut private

--- src/praktek03/Kerudung.java
+++ src/praktek03/Kerudung.java
@@ -1,9 +1,9 @@
 package praktek03;
 
 public class Kerudung {
-  String Merk;
-  String Warna;
-  Integer Harga;
+  private String Merk;
+  private String Warna;
+  private Integer Harga;
   
   void cetakInfo (){
       System.out.println("Merk \t: "+Merk+"\n"+"Warna \t: "+Warna+"\n"+"Harga \t: "+Harga);
