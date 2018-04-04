12bd5662537b6d449bca7d410e6d0f335217e8a9
hilmayunidamj1 <hilmayunidamj@gmail.com>
2018-04-04 06:34:43 -0700

    Buat Method Cetakinfo()

--- src/praktek03/Kerudung.java
+++ src/praktek03/Kerudung.java
@@ -1,7 +1,12 @@
 package praktek03;
 
 public class Kerudung {
-  String merk;
-  String warna;
+  String Merk;
+  String Warna;
   Integer Harga;
+  
+  void cetakInfo (){
+      System.out.println("Merk \t: "+Merk+"\n"+"Warna \t: "+Warna+"\n"+"Harga \t: "+Harga);
+      
+  }
 }
