3282b6d010067f44942b14eb2e294ced499f6c62
hilmayunidamj1 <hilmayunidamj@gmail.com>
2018-04-04 07:44:28 -0700

    atribute dan method

--- src/praktek03/BusanaMuslim.java
+++ src/praktek03/BusanaMuslim.java
@@ -1,5 +1,10 @@
 package praktek03;
 
 public class BusanaMuslim extends Kerudung{
+    private String jenisKerudung;
     
+    void throttle(){
+        System.out.println("KRESEK.....");
+    }
 }
+    
