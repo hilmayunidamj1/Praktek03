04d06c4658a74ab6f46e97172788f50b82041813
hilmayunidamj1 <hilmayunidamj@gmail.com>
2018-04-04 06:50:10 -0700

    Instance, buat atribut dan panggil method

--- src/praktek03/KerudungAksi.java
+++ src/praktek03/KerudungAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class KerudungAksi {
     public static void main(String[] args) {
+        Kerudung k = new Kerudung();
+        k.Merk="Arniz";
+        k.Warna="Hitam";
+        k.Harga=50000;
         
+        k.cetakInfo();
     }
 }
